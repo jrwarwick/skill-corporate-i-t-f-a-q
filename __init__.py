7dbda78f38188c11d2812121e664e63812742244
jrwarwick <justin.warwick@gmail.com>
2018-05-04 17:59:12 +0000

    Add checks for settings availability, and provide appropriate response in absence.

--- __init__.py
+++ __init__.py
@@ -6,29 +6,41 @@ class CorporateITFAQSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder().require('CorporateITFAQ'))
+    @intent_handler(IntentBuilder("CorporateITFAQIntent").require("CorporateITFAQ"))
     def handle_corporate_i_t_f_a_q(self, message):
-        self.speak_dialog('corporate.i.t.f.a.q')
+        self.speak_dialog("corporate.i.t.f.a.q")
 
-    @intent_handler(IntentBuilder().require('it_manager'))
+    @intent_handler(IntentBuilder("ITManagerIntent").require("it_manager"))
     def handle_it_manager(self, message):
-        data = {'it_manager': self.settings['it_manager']}
-        self.speak_dialog('it_manager',data)
+        if 'it_manager' not in self.settings or not self.settings['it_manager']:
+            self.speak_dialog("unknown")
+        else:
+            data = {'it_manager': self.settings['it_manager']}
+            self.speak_dialog("it_manager",data)
 
-    @intent_handler(IntentBuilder().require('vpn_address'))
+    @intent_handler(IntentBuilder("VpnAddressIntent").require("vpn_address"))
     def handle_vpn_address(self, message):
-        data = {'vpn_address': self.settings['vpn_address']}
-        self.speak_dialog('vpn_address',data=self.settings['vpn_address'])
+        if 'vpn_address'not in self.settings or not self.settings['vpn_address']:
+            self.speak_dialog("unknown")
+        else:
+            data = {'vpn_address': self.settings['vpn_address']}
+            self.speak_dialog("vpn_address",data=self.settings['vpn_address'])
 
-    @intent_handler(IntentBuilder().require('webmail_address'))
+    @intent_handler(IntentBuilder("WebmailAddressIntent").require("webmail_address"))
     def handle_webmail_address(self, message):
-        data = {'webmail_address': self.settings['webmail_address']}
-        self.speak_dialog('webmail_address',data=self.settings['webmail_address'])
+        if 'webmail_address' not in self.settings or not self.settings['webmail_address']:
+            self.speak_dialog("unknown")
+        else:
+            data = {'webmail_address': self.settings['webmail_address']}
+            self.speak_dialog("webmail_address",data=self.settings['webmail_address'])
 
-    @intent_handler(IntentBuilder().require('wifi_password'))
+    @intent_handler(IntentBuilder("WifiPasswordIntent").require("wifi_password"))
     def handle_wifi_password(self, message):
-        data = {'wifi_password': self.settings['wifi_password']}
-        self.speak_dialog('wifi_password',data=self.settings['wifi_password'])
+        if 'wifi_password' not in self.settings or not self.settings['wifi_password']:
+            self.speak_dialog("unknown")
+        else:
+            data = {'wifi_password': self.settings['wifi_password']}
+            self.speak_dialog("wifi_password",data=self.settings['wifi_password'])
 
 
 def create_skill():
