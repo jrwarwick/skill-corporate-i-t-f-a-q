c7fcd757b9e4900e470cc1cbd0273d046cc7e94e
jrwarwick <justin.warwick@gmail.com>
2018-05-04 20:15:16 +0000

    Add a couple more intents and fix data dict passing (already populated).

--- __init__.py
+++ __init__.py
@@ -6,9 +6,17 @@ class CorporateITFAQSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder("CorporateITFAQIntent").require("CorporateITFAQ"))
-    def handle_corporate_i_t_f_a_q(self, message):
-        self.speak_dialog("corporate.i.t.f.a.q")
+    def addressified(self, standard_string):
+        return standard_string.replace('.', ' dot ')
+
+
+    @intent_handler(IntentBuilder("WifiSsid").require("wifi_ssid"))
+    def handle_wifi_ssid(self, message):
+        if 'wifi_ssid' not in self.settings or not self.settings['wifi_ssid']:
+            self.speak_dialog("unknown")
+        else:
+            data = {'wifi_ssid': self.addressified(self.settings['wifi_ssid'])}
+            self.speak_dialog("wifi_ssid", data)
 
     @intent_handler(IntentBuilder("ITManagerIntent").require("it_manager"))
     def handle_it_manager(self, message):
@@ -16,23 +24,23 @@ class CorporateITFAQSkill(MycroftSkill):
             self.speak_dialog("unknown")
         else:
             data = {'it_manager': self.settings['it_manager']}
-            self.speak_dialog("it_manager",data)
+            self.speak_dialog("it_manager", data)
 
     @intent_handler(IntentBuilder("VpnAddressIntent").require("vpn_address"))
     def handle_vpn_address(self, message):
-        if 'vpn_address'not in self.settings or not self.settings['vpn_address']:
+        if 'vpn_address' not in self.settings or not self.settings['vpn_address']:
             self.speak_dialog("unknown")
         else:
-            data = {'vpn_address': self.settings['vpn_address']}
-            self.speak_dialog("vpn_address",data=self.settings['vpn_address'])
+            data = {'vpn_address': self.addressified(self.settings['vpn_address'])}
+            self.speak_dialog("vpn_address", data)
 
     @intent_handler(IntentBuilder("WebmailAddressIntent").require("webmail_address"))
     def handle_webmail_address(self, message):
         if 'webmail_address' not in self.settings or not self.settings['webmail_address']:
             self.speak_dialog("unknown")
         else:
-            data = {'webmail_address': self.settings['webmail_address']}
-            self.speak_dialog("webmail_address",data=self.settings['webmail_address'])
+            data = {'webmail_address': self.addressified(self.settings['webmail_address'])}
+            self.speak_dialog("webmail_address", data)
 
     ###@intent_handler(IntentBuilder("WifiPasswordIntent").optional("corporate").require("wifi_password"))
     @intent_handler(IntentBuilder("WifiPasswordIntent").require("wifi_password"))
@@ -41,15 +49,23 @@ class CorporateITFAQSkill(MycroftSkill):
             self.speak_dialog("unknown")
         else:
             data = {'wifi_password': self.settings['wifi_password']}
-            self.speak_dialog("wifi_password",data=self.settings['wifi_password'])
+            self.speak_dialog("wifi_password", data)
+
+    @intent_handler(IntentBuilder("IntranetWebsiteAddressIntent").require("intranet_website_address"))
+    def handle_intranet_website_address(self, message):
+        if 'intranet_website_address' not in self.settings or not self.settings['intranet_website_address']:
+            self.speak_dialog("unknown")
+        else:
+            data = {'intranet_website_address': self.addressified(self.settings['intranet_website_address'])}
+            self.speak_dialog("intranet_website_address", data)
 
     @intent_handler(IntentBuilder("KerberosDomainNameIntent").require("kerberos_domain_name"))
     def handle_kerberos_domain_name(self, message):
         if 'kerberos_domain_name' not in self.settings or not self.settings['kerberos_domain_name']:
             self.speak_dialog("unknown")
         else:
-            data = {'kerberos_domain_name': self.settings['kerberos_domain_name']}
-            self.speak_dialog("kerberos_domain_name",data=self.settings['kerberos_domain_name'])
+            data = {'kerberos_domain_name': addressified(self.settings['kerberos_domain_name'])}
+            self.speak_dialog("kerberos_domain_name", data)
 
 def create_skill():
     return CorporateITFAQSkill()
